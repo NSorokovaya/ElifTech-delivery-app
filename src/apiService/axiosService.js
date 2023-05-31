f031bfd0dc8e29a6ee9d36c6485b6533b06f709c
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-05-31 15:09:21 -0400

    1

--- src/apiService/axiosService.js
+++ src/apiService/axiosService.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = ''; // NEED TO ADD BASE_URL
+axios.defaults.baseURL = 'https://delivery-app-4o4c.onrender.com/'; // NEED TO ADD BASE_URL
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+// import { persistor, store } from 'redux/store';
+
 import { App } from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Provider>
+    <PersistGate loading={null}>
+      <BrowserRouter basename="kapusta-project">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
 );
